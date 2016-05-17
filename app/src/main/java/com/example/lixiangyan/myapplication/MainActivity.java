368cc9315867ed65e708c6725ab180453e6f47f0
xnfreedom <xnfreedom@163.com>
2016-05-17 17:29:29 +0800

    change commiter email

--- app/src/main/java/com/example/lixiangyan/myapplication/MainActivity.java
+++ app/src/main/java/com/example/lixiangyan/myapplication/MainActivity.java
@@ -20,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         //fab.setOnClickListener(new View.OnClickListener() {
+
         null.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
